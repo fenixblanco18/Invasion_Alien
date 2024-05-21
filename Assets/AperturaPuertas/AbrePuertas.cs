e1baf6366522d312abf13aaa2b0a4e0c2511a213
fenixblanco18 <montero.helena9@gmail.com>
2024-05-21 19:50:13 +0200

    Initial commit

---
+++ Assets/AperturaPuertas/AbrePuertas.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrePuertas : MonoBehaviour
+{
+    public string tagPlayer;
+    public Animator animatorPuerta;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagPlayer))
+        {
+            animatorPuerta.enabled = true;
+        }
+    }
+}
