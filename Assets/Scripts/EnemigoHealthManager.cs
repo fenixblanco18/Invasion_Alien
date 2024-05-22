2d37255e3d74a1c8a2b1530fd9166ee0fe180463
fenixblanco18 <montero.helena9@gmail.com>
2024-05-22 19:48:06 +0200

    animacion en slider del daño

--- Assets/Scripts/EnemigoHealthManager.cs
+++ Assets/Scripts/EnemigoHealthManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemigoHealthManager : MonoBehaviour
 {
+    public Slider slider;
     public int salud=100;
     public void HacerPupa(int pupa){
         salud-=pupa;
+        slider.value = salud;
     }
 }
--- Assets/Scripts/Proyectil.cs
+++ Assets/Scripts/Proyectil.cs
@@ -7,10 +7,10 @@ public class Proyectil : MonoBehaviour
     public int danyo = 1;
     void OnCollisionEnter(Collision collision)
     {
-        print(collision.gameObject.tag);
         if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponent<EnemigoHealthManager>().HacerPupa(danyo);
         }
+        Destroy(gameObject);
     }
 }
