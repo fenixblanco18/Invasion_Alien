054650d9543d48e45f377c0a4f223c9453d52f3f
fenixblanco18 <montero.helena9@gmail.com>
2024-05-24 16:56:22 +0200

    scripts necesarios, pero faltan ajustes

---
+++ Assets/Scripts/IniciarJuego.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IniciarJuego : MonoBehaviour
+{
+    // Start is called before the first frame updatepublic void IniciarJuego(){
+        
+        SceneManager.LoadScene(1);
+
+}
+
